c606fd8dfd91f8403c376bfc6c41c1502a03b710
sucheth19 <suchethg@usc.edu>
2023-09-04 09:33:46 -0700

    Plane Prototype

---
+++ Plane Prototype/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class FollowPlayerX : MonoBehaviour
+{
+    [SerializeField] private GameObject plane;
+    private Vector3 offset = new Vector3(30, 0, 10);
+    void Start() { }
+    void LateUpdate()
+    {
+        // The camera following the plane
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Plane Prototype/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float rotationSpeed;
+    public float verticalInput;
+    void Start() { }
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * verticalInput * rotationSpeed * Time.deltaTime);
+    }
+}
